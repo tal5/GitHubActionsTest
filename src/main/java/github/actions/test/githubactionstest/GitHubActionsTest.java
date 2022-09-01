65b2913954415dc9fb2138a9c69db530001528cc
Aya <31237389+tal5@users.noreply.github.com>
2022-09-01 14:40:26 +0300

    Add code to spawn a llama

--- src/main/java/github/actions/test/githubactionstest/GitHubActionsTest.java
+++ src/main/java/github/actions/test/githubactionstest/GitHubActionsTest.java
@@ -1,5 +1,8 @@
 package github.actions.test.githubactionstest;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Llama;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GitHubActionsTest extends JavaPlugin {
@@ -7,7 +10,9 @@ public final class GitHubActionsTest extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        World world = Bukkit.getWorlds().get(0);
+        world.spawn(world.getSpawnLocation(), Llama.class);
+        getLogger().info("Spawned llama at spawn.");
     }
 
     @Override
